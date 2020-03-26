cfe4c800bea344bdca29e5c5d148c60555eb8c65
Peter Holt <peter.holt@dochq.co.uk>
2020-03-26 09:22:48 +0000

    Added cells

--- src/table/cell.js
+++ src/table/cell.js
@@ -1,9 +1,33 @@
 import React from 'react';
 
-function Cell(props) {
-    return(
-        <td>{props.value}</td>
-    );
+class Cell extends React.Component {
+    items = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1];
+    elements = [];
+
+    constructor(props) {
+        super(props);
+        for (const [index, value] of this.items.entries()) {
+            this.elements.push(<option>{value}</option>)
+        }
+        this.state = {value: 0};
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({value: event.target.value});
+    }
+
+    render() {
+        return (
+            <td>
+                <div class="form-group">
+                    <select class="form-control" id="" onChange={this.handleChange}>
+                        {this.elements}
+                    </select>
+                </div>
+            </td>
+        );
+    }
 }
 
 export default Cell;
